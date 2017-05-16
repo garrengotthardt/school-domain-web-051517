5c8f0d8288691e83d5e0e67d4adfa20948e1c049
Garren Gotthardt <garren.x.gotthardt@gmail.com>
2017-05-16 18:11:05 -0400

    school domain

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,39 @@
 # code here!
+require "pry"
+
+class School
+  attr_accessor :roster, :grade
+  attr_reader :name
+
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+     @roster
+  end
+
+  def add_student(student_name, grade)
+    if @roster.key?(grade)
+      @roster[grade] << student_name
+    else
+        @roster[grade] = []
+        @roster[grade] << student_name
+    end
+    @roster
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, students_array|
+      students_array.sort!
+    end
+    @roster
+  end
+
+end
